6ea237fbef455b3def2cf47cdb629bda0759df01
Jazzkuh <jazzbauer77@gmail.com>
2022-07-25 17:00:12 +0200

    Add fallback instead of disabling plugin, might work, might not.

--- src/main/java/com/jazzkuh/gunshell/compatibility/CompatibilityManager.java
+++ src/main/java/com/jazzkuh/gunshell/compatibility/CompatibilityManager.java
@@ -1,8 +1,17 @@
 package com.jazzkuh.gunshell.compatibility;
 
 import com.jazzkuh.gunshell.GunshellPlugin;
+import com.jazzkuh.gunshell.api.objects.GunshellRayTraceResult;
 import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.FluidCollisionMode;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
+
+import java.util.Optional;
 
 public class CompatibilityManager {
     private final String bukkitVersion = Bukkit.getServer().getClass().getPackage().getName();
@@ -14,9 +23,53 @@ public class CompatibilityManager {
             GunshellPlugin.getInstance().getLogger().info("Using compatibility layer for version " + version);
             return (CompatibilityLayer) nmsClass.getConstructors()[0].newInstance();
         } catch (Exception ignored) {
-            GunshellPlugin.getInstance().getLogger().warning("Your server version (" + version + ") is not supported by Gunshell. Please upgrade to a supported version.");
-            Bukkit.getPluginManager().disablePlugin(GunshellPlugin.getInstance());
-            return null;
+            GunshellPlugin.getInstance().getLogger().warning("Your server version (" + version + ") is not supported by Gunshell. " +
+                    "Loading a fallback compatibility layer but this may cause issues so you are advised to update your server to the latest version.");
+            return new CompatibilityLayer() {
+                @Override
+                public GunshellRayTraceResult performRayTrace(LivingEntity player, double range) {
+                    RayTraceResult result = player.getWorld()
+                            .rayTrace(player.getEyeLocation(), player.getLocation().getDirection(), range, FluidCollisionMode.NEVER, true, 0.2, entity ->
+                                    entity != player);
+                    if (result == null) {
+                        return new GunshellRayTraceResult(Optional.empty(), Optional.empty(), null, false);
+                    }
+
+                    if (result.getHitBlock() != null) {
+                        return new GunshellRayTraceResult(Optional.empty(), Optional.of(result.getHitBlock()), result.getHitBlockFace(), false);
+                    }
+
+                    if (result.getHitEntity() == null) {
+                        return new GunshellRayTraceResult(Optional.empty(), Optional.empty(), null, false);
+                    }
+
+                    Entity entity = result.getHitEntity();
+                    if (!(entity instanceof LivingEntity) || entity instanceof ArmorStand) {
+                        return new GunshellRayTraceResult(Optional.empty(), Optional.empty(), null, false);
+                    }
+                    LivingEntity livingEntity = (LivingEntity) entity;
+                    boolean isHeadshot = (result.getHitPosition().getY() - entity.getLocation().getY()) > 1.375
+                            || (livingEntity instanceof Player && ((Player) livingEntity).isSneaking() && (result.getHitPosition().getY() - entity.getLocation().getY()) >  1.1);
+                    return new GunshellRayTraceResult(Optional.of(livingEntity), Optional.empty(), null, isHeadshot);
+                }
+
+                @Override
+                public String getRayTraceResult(Player player, int range) {
+                    RayTraceResult result = player.getWorld()
+                            .rayTrace(player.getEyeLocation(), player.getLocation().getDirection(), range, FluidCollisionMode.NEVER, true, 0.2, null);
+                    return result != null ? result.toString() : "No result found";
+                }
+
+                @Override
+                public void showEndCreditScene(Player player) {
+                    throw new UnsupportedOperationException("This server version is not supported by Gunshell");
+                }
+
+                @Override
+                public void showDemoMenu(Player player) {
+                    throw new UnsupportedOperationException("This server version is not supported by Gunshell");
+                }
+            };
         }
     }
 }

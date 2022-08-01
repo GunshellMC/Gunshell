e836ba50774d1dd5fcff17eb71da3e79f1d73c3e
Esmaybe <esmaybe@revenity.net>
2022-08-01 15:30:33 +0200

    Fixed indentation

--- src/main/java/com/jazzkuh/gunshell/common/listeners/ThrowablePreFireListener.java
+++ src/main/java/com/jazzkuh/gunshell/common/listeners/ThrowablePreFireListener.java
@@ -25,119 +25,119 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThrowablePreFireListener implements Listener {
-	@EventHandler
-	public void onThrowablePreFire(ThrowablePreFireEvent event) {
-		Player player = event.getPlayer();
-		GunshellThrowable throwable = event.getThrowable();
-		ItemStack itemStack = player.getInventory().getItemInMainHand();
+    @EventHandler
+    public void onThrowablePreFire(ThrowablePreFireEvent event) {
+        Player player = event.getPlayer();
+        GunshellThrowable throwable = event.getThrowable();
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
 
-		if (hasThrowableCooldown(player.getUniqueId(), throwable)) return;
-		GunshellPlugin.getInstance().getThrowableCooldownMap().put(player.getUniqueId(), System.currentTimeMillis());
+        if (hasThrowableCooldown(player.getUniqueId(), throwable)) return;
+        GunshellPlugin.getInstance().getThrowableCooldownMap().put(player.getUniqueId(), System.currentTimeMillis());
 
-		if (itemStack.getAmount() > 1) {
-			player.getInventory().getItemInMainHand().setAmount(itemStack.getAmount() - 1);
-		} else {
-			player.getInventory().removeItem(itemStack);
-		}
+        if (itemStack.getAmount() > 1) {
+            player.getInventory().getItemInMainHand().setAmount(itemStack.getAmount() - 1);
+        } else {
+            player.getInventory().removeItem(itemStack);
+        }
 
-		ArmorStand armorStand = player.getWorld().spawn(player.getLocation(), ArmorStand.class);
-		armorStand.setMetadata("gunshell_throwable_armorstand", new FixedMetadataValue(GunshellPlugin.getInstance(), true));
-		armorStand.setVelocity(player.getEyeLocation().getDirection().multiply(1.3D));
-		armorStand.setVisible(false);
-		armorStand.setSmall(true);
-		armorStand.setInvulnerable(true);
-		armorStand.getEquipment().setHelmet(
-				throwable.getItemStack());
+        ArmorStand armorStand = player.getWorld().spawn(player.getLocation(), ArmorStand.class);
+        armorStand.setMetadata("gunshell_throwable_armorstand", new FixedMetadataValue(GunshellPlugin.getInstance(), true));
+        armorStand.setVelocity(player.getEyeLocation().getDirection().multiply(1.3D));
+        armorStand.setVisible(false);
+        armorStand.setSmall(true);
+        armorStand.setInvulnerable(true);
+        armorStand.getEquipment().setHelmet(
+                throwable.getItemStack());
 
 
-		int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(GunshellPlugin.getInstance(), () -> {
-			int calculations = 20;
+        int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(GunshellPlugin.getInstance(), () -> {
+            int calculations = 20;
 
-			Vector velocity = armorStand.getVelocity();
-			Vector interval = velocity.clone().multiply(1.0 / calculations);
-			Location tempLocation = armorStand.getLocation().clone();
+            Vector velocity = armorStand.getVelocity();
+            Vector interval = velocity.clone().multiply(1.0 / calculations);
+            Location tempLocation = armorStand.getLocation().clone();
 
-			for (int i = 0; i < calculations; i++) {
-				Location tempLocationX = tempLocation.clone().add(interval.getX(), 0, 0);
-				Location tempLocationY = tempLocation.clone().add(0, interval.getY(), 0);
-				Location tempLocationZ = tempLocation.clone().add(0, 0, interval.getZ());
+            for (int i = 0; i < calculations; i++) {
+                Location tempLocationX = tempLocation.clone().add(interval.getX(), 0, 0);
+                Location tempLocationY = tempLocation.clone().add(0, interval.getY(), 0);
+                Location tempLocationZ = tempLocation.clone().add(0, 0, interval.getZ());
 
-				Block blockX = tempLocationX.getBlock();
-				Block blockY = tempLocationY.getBlock();
-				Block blockZ = tempLocationZ.getBlock();
+                Block blockX = tempLocationX.getBlock();
+                Block blockY = tempLocationY.getBlock();
+                Block blockZ = tempLocationZ.getBlock();
 
-				boolean xPassable = !GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockX) && !blockX.isLiquid();
-				boolean yPassable = !GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockY) && !blockY.isLiquid();
-				boolean zPassable = !GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockZ) && !blockZ.isLiquid();
+                boolean xPassable = !GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockX) && !blockX.isLiquid();
+                boolean yPassable = !GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockY) && !blockY.isLiquid();
+                boolean zPassable = !GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockZ) && !blockZ.isLiquid();
 
-				boolean xCollide = xPassable;
-				boolean yCollide = yPassable;
-				boolean zCollide = zPassable;
+                boolean xCollide = xPassable;
+                boolean yCollide = yPassable;
+                boolean zCollide = zPassable;
 
-				if (!xPassable && !yPassable) {
-					Block blockXY = tempLocation.clone().add(interval.getX(), interval.getY(), 0).getBlock();
-					if (!GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockXY) && !blockXY.isLiquid()) {
-						xCollide = true;
-						yCollide = true;
-					}
-				}
+                if (!xPassable && !yPassable) {
+                    Block blockXY = tempLocation.clone().add(interval.getX(), interval.getY(), 0).getBlock();
+                    if (!GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockXY) && !blockXY.isLiquid()) {
+                        xCollide = true;
+                        yCollide = true;
+                    }
+                }
 
-				if (!xPassable && !zPassable) {
-					var blockXZ = tempLocation.clone().add(interval.getX(), 0, interval.getZ()).getBlock();
-					if (!GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockXZ) && !blockXZ.isLiquid()) {
-						xCollide = true;
-						zCollide = true;
-					}
-				}
+                if (!xPassable && !zPassable) {
+                    var blockXZ = tempLocation.clone().add(interval.getX(), 0, interval.getZ()).getBlock();
+                    if (!GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockXZ) && !blockXZ.isLiquid()) {
+                        xCollide = true;
+                        zCollide = true;
+                    }
+                }
 
-				if (!yPassable && !zPassable) {
-					var blockYZ = tempLocation.clone().add(0, interval.getY(), interval.getZ()).getBlock();
-					if (!GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockYZ) && !blockYZ.isLiquid()) {
-						yCollide = true;
-						zCollide = true;
-					}
-				}
+                if (!yPassable && !zPassable) {
+                    var blockYZ = tempLocation.clone().add(0, interval.getY(), interval.getZ()).getBlock();
+                    if (!GunshellPlugin.getInstance().getCompatibilityLayer().isPassable(blockYZ) && !blockYZ.isLiquid()) {
+                        yCollide = true;
+                        zCollide = true;
+                    }
+                }
 
-				if (xCollide) {
-					velocity.setX(-0.6 * velocity.getX());
-					interval.setX(-0.6 * interval.getX());
-				}
+                if (xCollide) {
+                    velocity.setX(-0.6 * velocity.getX());
+                    interval.setX(-0.6 * interval.getX());
+                }
 
-				if (yCollide) {
-					velocity.setY(-0.6 * velocity.getY());
-					interval.setY(-0.6 * interval.getY());
-				}
+                if (yCollide) {
+                    velocity.setY(-0.6 * velocity.getY());
+                    interval.setY(-0.6 * interval.getY());
+                }
 
-				if (zCollide) {
-					velocity.setZ(-0.6 * velocity.getZ());
-					interval.setZ(-0.6 * interval.getZ());
-				}
+                if (zCollide) {
+                    velocity.setZ(-0.6 * velocity.getZ());
+                    interval.setZ(-0.6 * interval.getZ());
+                }
 
-				tempLocation.add(interval);
-				armorStand.setVelocity(velocity);
-			}
-		}, 5, 1);
+                tempLocation.add(interval);
+                armorStand.setVelocity(velocity);
+            }
+        }, 5, 1);
 
-		GunshellPlugin.getInstance().getActiveThrowables().put(armorStand, taskId);
-		Bukkit.getScheduler().runTaskLater(GunshellPlugin.getInstance(), () -> {
-			GunshellPlugin.getInstance().getActiveThrowables().remove(armorStand);
-			Bukkit.getScheduler().cancelTask(taskId);
+        GunshellPlugin.getInstance().getActiveThrowables().put(armorStand, taskId);
+        Bukkit.getScheduler().runTaskLater(GunshellPlugin.getInstance(), () -> {
+            GunshellPlugin.getInstance().getActiveThrowables().remove(armorStand);
+            Bukkit.getScheduler().cancelTask(taskId);
 
-			Location location = armorStand.getLocation();
-			armorStand.remove();
+            Location location = armorStand.getLocation();
+            armorStand.remove();
 
-			ThrowableActionImpl throwableAction = ThrowableActionRegistry.getAction(throwable, throwable.getActionType());
-			if (throwableAction == null) {
-				ChatUtils.sendMessage(player, "&cNo throwable action found for this throwable.");
-				return;
-			}
+            ThrowableActionImpl throwableAction = ThrowableActionRegistry.getAction(throwable, throwable.getActionType());
+            if (throwableAction == null) {
+                ChatUtils.sendMessage(player, "&cNo throwable action found for this throwable.");
+                return;
+            }
 
-			throwableAction.fireAction(player, location, throwable.getConfiguration());
-		}, throwable.getFuseTime());
-	}
+            throwableAction.fireAction(player, location, throwable.getConfiguration());
+        }, throwable.getFuseTime());
+    }
 
-	private boolean hasThrowableCooldown(UUID uniqueId, GunshellThrowable throwable) {
-		Long lastUsed = GunshellPlugin.getInstance().getThrowableCooldownMap().getOrDefault(uniqueId, 0L);
-		return System.currentTimeMillis() <= (lastUsed + (throwable.getCooldown()));
-	}
+    private boolean hasThrowableCooldown(UUID uniqueId, GunshellThrowable throwable) {
+        Long lastUsed = GunshellPlugin.getInstance().getThrowableCooldownMap().getOrDefault(uniqueId, 0L);
+        return System.currentTimeMillis() <= (lastUsed + (throwable.getCooldown()));
+    }
 }

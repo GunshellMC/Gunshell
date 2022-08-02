b193bc694e3209facd2d473124b868a92c09e96d
Jazzkuh <jazzbauer77@gmail.com>
2022-08-02 16:06:10 +0200

    Hex colors

--- src/main/java/com/jazzkuh/gunshell/utils/ChatUtils.java
+++ src/main/java/com/jazzkuh/gunshell/utils/ChatUtils.java
@@ -1,16 +1,31 @@
 package com.jazzkuh.gunshell.utils;
 
 import com.jazzkuh.gunshell.common.configuration.PlaceHolder;
+import com.jazzkuh.gunshell.compatibility.CompatibilityManager;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChatUtils {
+    public static final Pattern hexPattern = Pattern.compile("&#(\\w{5}[0-9a-f])");
     public static String color(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
+        if (!CompatibilityManager.getVersion().equals("v1_12_R1")) {
+            Matcher matcher = hexPattern.matcher(message);
+            StringBuffer stringBuilder = new StringBuffer();
+
+            while (matcher.find()) {
+                matcher.appendReplacement(stringBuilder, net.md_5.bungee.api.ChatColor.of("#" + matcher.group(1)).toString());
+            }
+
+            return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(stringBuilder).toString());
+        } else {
+            return ChatColor.translateAlternateColorCodes('&', message);
+        }
     }
 
     public static List<String> color(List<String> lore) {
